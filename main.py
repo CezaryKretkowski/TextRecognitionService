edc773a8c9405752dd31f17d1a34e0286c00358c
cezary <cezarykretkowski@gmail.com>
2024-06-02 12:32:14 +0200

    fixees

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ nlp = spacy.load('pl_core_news_sm')
 app = FastAPI()
 class ReguestModel(BaseModel):
     text:str
-    separator:str
 
 
 @app.post("/GetRecords/")
 async def get_records(item:ReguestModel):
-    return get_names(item.text,item.separator)
+    return get_names(item.text,'pacjent')
 
 def extract_names(text):
     doc = nlp(text)
@@ -39,12 +38,34 @@ def find_last_name(text, target_word):
         return None
 def get_names(text,separator):
 
+    text = normalize_text(text);
     date = text.split(separator)
     entities = []
     for i in date:
         if i != "":
             name = extract_names(i)
-            last_name = find_last_name(i, name)
+            if ' ' in name:
+                args = name.split(' ')
+                if len(args) == 2:
+                    name = args[0]
+                    last_name= args [1];
+                else:
+                     last_name = find_last_name(i, name)
+            else:
+               last_name = find_last_name(i, name)
             ids = get_code(i)
             entities.append( {"name":name, "lastName":last_name,"id":ids, "text":i})
     return entities
+
+
+def normalize_text(text):
+    corrections = {
+        r'pacient': 'pacjent',
+        r'pacięt': 'pacjent',
+        r'pacienc': 'pacjent',
+        r'pacjęt': 'pacjent',
+        r'pacjęnt': 'pacjent'
+    }
+    for pattern, correction in corrections.items():
+        text = re.sub(pattern, correction, text, flags=re.IGNORECASE)
+    return text
